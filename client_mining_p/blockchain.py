2f24f72d642f133bc426ad560e82be09db06d836
VaderSteve76 <alverson.s@yahoo.com>
2019-07-11 21:12:52 -0400

    Miner Working

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -4,6 +4,8 @@ from time import time
 from uuid import uuid4
 
 from flask import Flask, jsonify, request
+from urllib.parse import urlparse
+import requests
 
 import sys
 
@@ -13,8 +15,19 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
         self.nodes = set()
+        self.genesis_block()
 
-        self.new_block(previous_hash=1, proof=100)
+    def genesis_block(self):
+
+        block = {
+            'index': 1,
+            'timestamp': 0,
+            'transactions': [],
+            'proof': 99,
+            'previous_hash': 1
+        }
+
+        self.chain.append(block)
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -38,6 +51,10 @@ class Blockchain(object):
 
         self.chain.append(block)
         return block
+
+    def add_block(self, block):
+        self.current_transactions = []
+        self.chain.append(block)
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -129,6 +146,41 @@ class Blockchain(object):
 
         return True
 
+    def register_nodes(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get('http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False
+
+    def broadcast_new_block(self, block):
+        neighbours = self.nodes
+        post_data = {'block': block}
+
+        for node in neighbours:
+            response = requests.post('http://{node}/block/new', json=post_data)
+
+            if response.status_code != 200:
+                pass
+
 
 # Instantiate our Node
 app = Flask(__name__)
@@ -145,28 +197,62 @@ def mine():
     # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
     last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
+    values = request.get_json()
+    submitted_proof = values.get('proof')
 
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mine a new coin
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
+    if blockchain.valid_proof(last_proof, submitted_proof):
+        # We must receive a reward for finding the proof.
+        # The sender is "0" to signify that this node has mine a new coin
+        blockchain.new_transaction(
+            sender="0",
+            recipient=node_identifier,
+            amount=1,
+        )
 
-    # Forge the new BLock by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
+        # Forge the new BLock by adding it to the chain
+        previous_hash = blockchain.hash(last_block)
+        block = blockchain.new_block(submitted_proof, previous_hash)
+        blockchain.broadcast_new_block(block)
 
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
+        response = {
+            'message': "New Block Forged",
+            'index': block['index'],
+            'transactions': block['transactions'],
+            'proof': block['proof'],
+            'previous_hash': block['previous_hash'],
+        }
+        return jsonify(response), 200
+    else:
+        response = {
+            'message': 'Not valid proof'
+        }
+        return jsonify(response), 200
+
+
+@app.route('/block/new', methods=['POST'])
+def new_block():
+    values = request.get_json()
+    required = ['block']
+    if not all(k in values for k in required):
+        return 'No values', 400
+
+        new_block = values.get('block')
+        old_block = blockchain.last_block
+        print('block received', file=sys.stderr)
+        print('with index' + str(new_block.get('index')), file=sys.stderr)
+        if new_block.get('index') == old_block('index') + 1:
+            print('has correct index', file=sys.stderr)
+            if (new_block.get('previous_hash') ==
+                    blockchain.hash(blockchain.last_block)):
+                print('new block accepted', file=sys.stderr)
+                blockchain.add_block(new_block)
+            return 'Block Accepted', 200
+        else:
+            return 'Not valid, incorrect hash', 400
+    else:
+        print('finding consensus', file=sys.stderr)
+        consensus()
+        return 'Finding consensus', 200
 
 
 @app.route('/transactions/new', methods=['POST'])
@@ -196,9 +282,52 @@ def full_chain():
     return jsonify(response), 200
 
 
+@app.route('/last_proof', methods=['GET'])
+def last_proof():
+    last_proof_value = blockchain.last_block.get('proof')
+    response = {
+        'proof': last_proof_value
+    }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+    if nodes is None:
+        return 'Provide the correct list', 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes added',
+        'total_nodes': list(blockchain.nodes)
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Replaced chain',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Chain is authoritative',
+            'chain': blockchain.chain
+        }
+    return jsonify(response), 200
+
 # Note, when demoing, start with this, then change to the below
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,7 +4,21 @@ import requests
 import sys
 
 
-# TODO: Implement functionality to search for a proof 
+# TODO: Implement functionality to search for a proof
+
+def proof_of_work(last_proof):
+    print('Looking for next proof')
+    proof = 0
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
+    print('Found proof: ' + str(proof))
+    return proof
+
+
+def valid_proof(last_proof, proof):
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == '000000'
 
 
 if __name__ == '__main__':
@@ -12,14 +26,21 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         node = sys.argv[1]
     else:
-        node = "http://localhost:5000"
+        node = "http://0.0.0.0:5000"
 
     coins_mined = 0
     # Run forever until interrupted
     while True:
-        # TODO: Get the last proof from the server and look for a new one
-        # TODO: When found, POST it to the server {"proof": new_proof}
-        # TODO: If the server responds with 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        pass
+        r = requests.get(url=node + '/last_proof')
+        data = r.json()
+        new_proof = proof_of_work(data.get('proof'))
+
+        post_data = {"proof": new_proof}
+
+        r = requests.get(url=node + '/mine', json=post_data)
+        data = r.json()
+        if data.get('message') == 'New Block Forged':
+            coins_mined += 1
+            print("Total coins mined: " + str(coins_mined))
+        else:
+            print(data.get('message'))
